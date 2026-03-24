11afd6b883674885c464e1e332fba67baa58f0f0
David Lerner <david.lerner@hotmail.com>
2026-03-24 18:43:52 -0300

    Add multiple icons for PWA

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
 import { VitePWA } from 'vite-plugin-pwa';
 
 const manifestForPlugin = {
   registerType: 'prompt',
-  includeAssests: ['favicon.ico', 'apple-touc-icon.png', 'masked-icon.svg'],
+  includeAssets: ['favicon.ico', 'apple-touc-icon.png', 'masked-icon.svg'],
   manifest: {
-    name: 'Find-your-beauty',
-    short_name: 'find-your-beauty',
-    description: 'Find beauty saloons near you',
+    name: 'Crown-clothing',
+    short_name: 'crown-clothing',
+    description: 'Shop your favorite clothes without leaving your house.',
     icons: [
       {
-        src: '/android-chrome-192x192.png',
+        src: 'favicon.ico',
+        sizes: '64x64 32x32 24x24 16x16',
+        type: 'image/x-icon',
+      },
+      {
+        src: 'logo-192x192.png',
+        type: 'image/png',
         sizes: '192x192',
-        type: 'image/png',
-        purpose: 'favicon',
+        purpose: 'any maskable',
       },
       {
-        src: '/android-chrome-512x512.png',
+        src: 'logo-512x512.png',
+        type: 'image/png',
         sizes: '512x512',
-        type: 'image/png',
-        purpose: 'favicon',
-      },
-      {
-        src: '/apple-touch-icon.png',
-        sizes: '180x180',
-        type: 'image/png',
-        purpose: 'apple touch icon',
-      },
-      {
-        src: '/maskable_icon.png',
-        sizes: '512x512',
-        type: 'image/png',
         purpose: 'any maskable',
       },
     ],
-    theme_color: '#181818',
-    background_color: '#e0cc3b',
     display: 'standalone',
+    theme_color: '#000000',
+    background_color: '#ffffff',
     scope: '/',
     start_url: '/',
     orientation: 'portrait',
@@ -46,5 +38,5 @@ const manifestForPlugin = {
 };
 
 export default defineConfig({
-  plugins: [react(), svgr(), VitePWA(manifestForPlugin)],
+  plugins: [react(), VitePWA(manifestForPlugin)],
 });

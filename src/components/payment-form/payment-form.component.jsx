0c7a9d08aded6c649c35312c657d49b30624cac8
David Lerner <david.lerner@hotmail.com>
2026-03-24 23:44:32 -0300

    Fix amount

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -31,7 +31,7 @@ const PaymentForm = () => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ amount: 1 }),
+      body: JSON.stringify({ amount: 100 }),
     }).then((res) => {
       return res.json();
     });

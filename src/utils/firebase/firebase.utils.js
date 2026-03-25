8bfcaf4ed497f085008eecf5933f3bb716108e6e
David Lerner <david.lerner@hotmail.com>
2026-03-24 23:21:41 -0300

    Remove console logs

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -34,14 +34,14 @@ export const auth = getAuth();
 
 export const signInWithGoogle = () => {
   signInWithPopup(auth, provider)
-    .then((result) => {
+    .then(() => {
       // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      console.log(token);
+      // const credential = GoogleAuthProvider.credentialFromResult(result);
+      // const token = credential.accessToken;
+      // console.log(token);
       // The signed-in user info.
-      const user = result.user;
-      console.log(user);
+      // const user = result.user;
+      // console.log(user);
       // IdP data available using getAdditionalUserInfo(result)
       // ...
     })
